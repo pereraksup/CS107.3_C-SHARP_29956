fd90eba66235a1a87240631653cdeeed4d16cb86
pereraksup <135945510+pereraksup@users.noreply.github.com>
2024-04-02 15:15:03 +0530

    Add files via upload

---
+++ lab 04 2024.02.14 github/lab sheet 03 Q1/CourseClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_sheet_03_Q1
+{
+    internal class CourseClass
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter course name:");
+            string courseName = Console.ReadLine();
+
+            Console.Write("Enter instructor name: ");
+            string instructorName = Console.ReadLine();
+
+            Console.WriteLine("Enter grade (0-100): ");
+            double grade = double.Parse(Console.ReadLine());
+
+
+            Course course = new Course(courseName);
+
+            course.SetInstructorName(instructorName);
+            course.Grade = grade;
+
+            course.PrintCourseInfo();
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
---
+++ lab 04 2024.02.14 github/lab sheet 03 Q1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_sheet_03_Q1
+{
+    public class Course
+    {
+        private string courseName;
+        private string instructorName;
+        private double grade;
+
+        public Course(string courseName)
+        {
+            if (string.IsNullOrEmpty(courseName))
+            {
+                throw new ArgumentNullException("course name cannot be empty. ");
+            }
+            this.courseName = courseName;
+        }
+
+
+        public string CourseName => courseName; // read only property course
+
+        public double Grade
+        {
+            get => grade;
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    grade = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Grade must be between 0 and 100");
+                }
+            }
+        }
+
+
+        public void SetInstructorName(string instructorName) //methode to get instructor
+        {
+            if (!string.IsNullOrEmpty(instructorName))
+            {
+                throw new ArgumentException("instructor Name");
+            }
+            this.instructorName = instructorName;
+        }
+        private string CalculateGrade()
+        {
+            if (grade >= 90)
+            {
+                return "A";
+            }
+            else if (grade >= 80)
+            {
+                return "B";
+            }
+            else if (grade >= 70)
+            {
+                return "C";
+            }
+            else if (grade >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+
+        }
+        public void PrintCourseInfo() //method to print course information
+        {
+            Console.WriteLine($"Course Name: {courseName} ");
+            Console.WriteLine($"Instructor Name: {instructorName}");
+            Console.WriteLine($"Letter Grade: {CalculateGrade()}");
+        }
+
+    }
+}
